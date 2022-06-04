c3dfc62428871ab0b5065be862524d9706f96d5a
Jake Walsh <jkwalsh127@gmail.com>
2022-06-03 20:34:58 -0700

    initiate schedule controller

---
+++ server/controllers/scheduleController.js
@@ -0,0 +1,15 @@
+const { Schedule } = require('../models');
+
+module.exports = {
+  // Get a single schedule
+  getSingleSchedule(req, res) {
+    Schedule.findOne({ _id: req.params.scheduleId })
+      .select('-__v')
+      .then((schedule) =>
+        !schedule
+          ? res.status(404).json({ message: 'No schedule with that ID' })
+          : res.json(schedule)
+      )
+      .catch((err) => res.status(500).json(err));
+  }
+};
