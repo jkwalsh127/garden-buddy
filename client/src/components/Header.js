2112c59af33d45e7b66cb0888b253beb156ee9ad
Jake Walsh <jkwalsh127@gmail.com>
2022-06-04 09:42:00 -0700

    style header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './styles/Header.css';
+import './styles/header.css';
 // import './styles/HeaderMediaQueries.css';
 
 // import Auth from '../../utils/auth';
@@ -11,28 +11,26 @@ const Header = () => {
 //     Auth.logout();
 //   };
   return (
-    <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Garden Buddy
-          </h1>
-        </Link>
-        <div>
-          {/* {Auth.loggedIn() ? ( */}
-            <>
-              <Link className="btn btn-lg btn-primary m-2" to="/findzone">
-                Find Zone
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/profile">
-                View Profile
-              </Link>
-            </>
-          {/*}) : (
-            <>
-            </>
-          )}*/}
-        </div>
+    <header className="header">
+      <Link className="" to="/">
+        <h1 className="app-title">
+          Garden Buddy
+        </h1>
+      </Link>
+      <div className='nav'>
+        {/* {Auth.loggedIn() ? ( */}
+          <>
+            <Link className="nav-link" to="/findzone">
+              Find Zone
+            </Link>
+            <Link className="nav-link" to="/profile">
+              View Profile
+            </Link>
+          </>
+        {/*}) : (
+          <>
+          </>
+        )}*/}
       </div>
     </header>
   );
