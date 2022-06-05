a3e99e747d81cd30d2f66b5756fdd6525b41fb18
Jake Walsh <jkwalsh127@gmail.com>
2022-06-05 09:57:20 -0700

    add user to queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,26 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      gardens {
+        _id
+        vegetable
+        variety
+        startedAs
+        sowDate
+        plantDate
+        firstHarvest
+        lastHarvest
+        notes
+      }
+    }
+  }
+`;
+
 export const QUERY_GARDENS = gql`
   query garden {
     garden {
