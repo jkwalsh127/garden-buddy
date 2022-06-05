eb8d7062bb589c638018f72f2c664eaa568f3e55
Jake Walsh <jkwalsh127@gmail.com>
2022-06-04 18:54:49 -0700

    configure client to run in coordination with server. Also initiate landing page

--- client/src/app.js
+++ client/src/app.js
@@ -1,30 +1,44 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Schedule from './components/pages/Schedule';
 import Profile from './components/pages/Profile';
+import Landing from './components/pages/Landing';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className='outer-container'>
-          <div className='inner-container'>
-            <Routes>
-              <Route 
-                  path="/findzone" 
-                  element={<Schedule />}
-              />
-              <Route 
-                  path="/myprofile" 
-                  element={<Profile />}
-              />
-            </Routes>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className='outer-container'>
+            <div className='inner-container'>
+              <Routes>
+                <Route 
+                    path="/" 
+                    element={<Landing />}
+                />
+                <Route 
+                    path="/findzone" 
+                    element={<Schedule />}
+                />
+                <Route 
+                    path="/myprofile" 
+                    element={<Profile />}
+                />
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
