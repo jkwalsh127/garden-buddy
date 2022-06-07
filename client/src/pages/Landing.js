fc999fb7fdd4d76f247e252f82925da2fdcc2e41
Snehita Kolli <snehitakolli20@gmail.com>
2022-06-07 11:43:40 -0700

    npm installs to confirm everything is uptodate

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/landing.css';
 // import Signup from '../components/signup';
 
-export default function Schedule() {
+export default function Landing() {
     return (
         <>
             <h1 className='page-title'>Welcome</h1>
