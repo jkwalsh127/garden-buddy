28562e60cfbd2e10d8e1d71c89d8a8ec0172bb57
Snehita Kolli <snehitakolli20@gmail.com>
2022-06-07 12:24:37 -0700

    landing page formatted for box

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './styles/landing.css';
-// import Signup from '../components/signup';
+import { Row, Col } from 'react-bootstrap';
 
 export default function Landing() {
     return (
         <>
-            <h1 className='page-title'>Welcome</h1>
+            <h1 className='page-title'>Welcome to Garden Buddy!</h1>
+            <Row>
+                <Col xs={{ span: 10, offset: 1 }} md={{ span: 7, offset: 1 }}>
+                    <p className="about-text">
+                       Your one-stop-shop for all things gardening! 
+                    </p>
+                </Col>
+        </Row>
         </>
     );
 }
