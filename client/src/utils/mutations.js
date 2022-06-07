d3641754f1d6d788546523bb43f5269cc73733e7
Snehita Kolli <snehitakolli20@gmail.com>
2022-06-07 13:39:50 -0700

    changed to username in both user model and mutations folder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,12 +14,12 @@ mutation login($email: String!, $password: String!) {
 `;
 
 export const ADD_USER = gql`
-mutation addUser($name: String!, $email: String!, $password: String!) {
-  addUser(username: $name, email: $email, password: $password) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
     token
     user {
       _id
-      name
+      username
       email
       password
     }
