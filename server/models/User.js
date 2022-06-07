f58bd01cda7f76d9cb77ab0c44bc81539ed44386
Snehita Kolli <snehitakolli20@gmail.com>
2022-06-07 13:38:22 -0700

    user model change for name to username

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    name: {
+    userName: {
         type: String, 
         required: true, 
         unique: true, 
