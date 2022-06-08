dfedc0fb589f76e7344ec7a868044f89e92ec990
Jake Walsh <jkwalsh127@gmail.com>
2022-06-07 21:02:35 -0700

    general edits

--- client/src/pages/Gardens.js
+++ client/src/pages/Gardens.js
@@ -8,6 +8,7 @@ import './styles/gardens.css';
 import { useQuery } from '@apollo/client';
 import { QUERY_GARDENS } from '../utils/queries';
 // import { CREATE_GARDEN } from '../../utils/mutations';
+import GardenTable from '../components/GardenTable';
 
 const Profile = () => {
     const { loading, data } = useQuery(QUERY_GARDENS);
@@ -42,6 +43,7 @@ const Profile = () => {
         <div>Loading...</div>
           ) : (
           <div>
+            <GardenTable />
             <label>Garden 1: </label>
             {gardenList.map((garden) => {
               return (
