6a567031d3e72710d7c8bf046bd61e84196f595c
Jake Walsh <jkwalsh127@gmail.com>
2022-06-09 12:42:49 -0700

    introduce images to footer and page titles

--- client/src/pages/Gardens.js
+++ client/src/pages/Gardens.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './styles/gardens.css';
 import GardenTable from '../components/GardenTable';
+import birdLeft from './images/bird-left.png';
+import birdRight from './images/bird-right.png';
 
 const Profile = () => {
 
@@ -10,7 +12,11 @@ const Profile = () => {
 
     return (
       <div className=''>
-        <h1 className='page-title'>My Gardens</h1>
+        <div className='title-wrap'>
+          <img src={birdLeft} alt="birds" className='birds'/>
+          <h1 className='page-title'>My Gardens</h1>
+          <img src={birdRight} alt="birds" className='birds'/>
+        </div>
         <div className=''>
 
           <GardenTable />
