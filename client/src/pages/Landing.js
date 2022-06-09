280e1ec4e66af233ef51c2696772b44e97c497b6
toma <toma95s@tomas-MBP.lan>
2022-06-09 16:07:44 -0700

    added new font family to header and to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/landing.css';
 import { Row, Col, Button } from 'react-bootstrap';
-
+import fruits from './images/fruits.jpg';
+import veggies from './images/veggies.jpg'
 
 export default function Landing() {
     return (
         <div>
             <h1 className='page-title'>Welcome to Garden Buddy!</h1>
+            <img src={fruits} alt="fruits" className='fruits' />
             <Row>
                 <Col xs={{ span: 10, offset: 2 }} md={{ span: 7, offset: 2 }}>
                     <p className="about-text">
@@ -18,11 +20,12 @@ export default function Landing() {
                 <a href="/login" rel="noreferrer">
                     <Button id="button">Login</Button>
                 </a>
-
                 <a href="/signup" rel="noreferrer">
                     <Button id="button">Signup</Button>
                 </a>
             </div>
+            <img src={veggies} alt="fruits" className='veggies'/> 
         </div>
+        
     );
 }
