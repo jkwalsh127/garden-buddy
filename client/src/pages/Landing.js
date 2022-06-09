6b98c56da6ced0ea4c2869e5a3b841807405f69e
toma <toma95s@tomas-MBP.lan>
2022-06-09 16:56:17 -0700

    changed submit button and zipcode input areas font

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/landing.css';
 import { Row, Col, Button } from 'react-bootstrap';
 import fruits from './images/fruits.jpg';
-import veggies from './images/veggies.jpg'
+import veggies from './images/veggies.jpg';
 
 export default function Landing() {
     return (
