3c23bb74ab08620351b3621d3f4e135cd1b2ac6d
Snehita Kolli <snehitakolli20@gmail.com>
2022-06-10 09:49:01 -0700

    add eggplants and tomato info for comps

--- client/src/components/Eggplants.js
+++ client/src/components/Eggplants.js
@@ -38,7 +38,7 @@ export default function Eggplants() {
                             </li>
                             <label className='detail-label'>Soil pH</label>
                             <li className='detail'>
-                                Acidic-Neutral
+                                Acidic, neutral, alkaline
                             </li>
                             <label className='detail-label'>Bloom Time</label>
                             <li className='detail'>
@@ -46,7 +46,7 @@ export default function Eggplants() {
                             </li>
                             <label className='detail-label'>Days to Harvest</label>
                             <li className='detail'>
-                                60-100
+                                80-100
                             </li>
                             <label className='detail-label'>Recommended Start</label>
                             <li className='detail'>
@@ -61,7 +61,7 @@ export default function Eggplants() {
                     <div className='description-wrap'>
                         <img className='veggie-img' src={eggplant} alt='tomato plant'></img>
                         <p className='description'>
-                            THSSI ISHI HDIHF OSDHFDO ISHF ODIHFSDOIF HDOIHDFO IHFDOID FHODISF. THSS IISH IHD IHFO SDHFDO ISH FODIHFSDOIF HDOIH DFOIH FDOID FHODI SF.THSSIIS HIHD IHFOS DHFDOI SHF ODIHFS DOIFHD OIHDFOI HFDOIDF HODISF. THSSII SHIHD IHFOSD HFDOI SHFODI HFSDO IFHDOI HDFOI HFDOID FHOD ISF.THSSI ISHI HDIH FOSDH FDOISH FODIHF SDOIFH DOIH DFOIHF DOIDFHOD ISF. THSSI ISH IHDIH FOSDHF DOIS HFODIH FSDOIFHD OIHD FOIHFDO IDFHO DISF.
+                            Eggplants can be used in a variety of ways. 
                         </p>
                     </div>
                 </div>
--- client/src/components/Tomatoes.js
+++ client/src/components/Tomatoes.js
@@ -61,7 +61,7 @@ export default function Tomatoes() {
                     <div className='description-wrap'>
                         <img className='veggie-img' src={tomato} alt='tomato plant'></img>
                         <p className='description'>
-                            THSSI ISHI HDIHF OSDHFDO ISHF ODIHFSDOIF HDOIHDFO IHFDOID FHODISF. THSS IISH IHD IHFO SDHFDO ISH FODIHFSDOIF HDOIH DFOIH FDOID FHODI SF.THSSIIS HIHD IHFOS DHFDOI SHF ODIHFS DOIFHD OIHDFOI HFDOIDF HODISF. THSSII SHIHD IHFOSD HFDOI SHFODI HFSDO IFHDOI HDFOI HFDOID FHOD ISF.THSSI ISHI HDIH FOSDH FDOISH FODIHF SDOIFH DOIH DFOIHF DOIDFHOD ISF. THSSI ISH IHDIH FOSDHF DOIS HFODIH FSDOIFHD OIHD FOIHFDO IDFHO DISF.
+                            Just a general guide on tomatoes! Can be used in a variety of ways. 
                         </p>
                     </div>
                 </div>
