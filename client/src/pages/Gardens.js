067718e06f3e6b37af92573c61a297d6f127165d
Jake Walsh <jkwalsh127@gmail.com>
2022-06-09 20:16:54 -0700

    format gardens page head

--- client/src/pages/Gardens.js
+++ client/src/pages/Gardens.js
@@ -5,6 +5,8 @@ import './styles/gardens.css';
 import GardenTable from '../components/GardenTable';
 import birdLeft from './images/bird-left.png';
 import birdRight from './images/bird-right.png';
+import tomatoesLeft from './images/tomatoes-left.png';
+import tomatoesRight from './images/tomatoes-right.png';
 
 const Profile = () => {
 
@@ -12,10 +14,14 @@ const Profile = () => {
 
     return (
       <div className='gardens-wrap'>
-        <div className='title-wrap-gardens'>
-          <img src={birdLeft} alt="birds" className='birds'/>
-          <h1 className='page-title'>My Gardens</h1>
-          <img src={birdRight} alt="birds" className='birds'/>
+        <div className='head-wrap'>
+          <img src={tomatoesLeft} alt="birds" className='tomatoes'/>
+          <div className='title-wrap-gardens'>
+            <img src={birdLeft} alt="birds" className='birds'/>
+            <h1 className='page-title'>My Gardens</h1>
+            <img src={birdRight} alt="tomatoes" className='birds'/>
+          </div>
+          <img src={tomatoesRight} alt="tomatoes" className='tomatoes'/>
         </div>
         <GardenTable />
       </div>
