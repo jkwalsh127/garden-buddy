ae776b8aa39aa9fd962fdb65647ec8ec28371f55
Jake Walsh <jkwalsh127@gmail.com>
2022-06-11 18:42:05 -0700

    format login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import './styles/login.css';
 
 import Auth from '../utils/auth';
 
@@ -41,29 +42,31 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
+    <main className="login-container">
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <h4 className="card-head">Login</h4>
           <div className="card-body">
             {data ? (
-              <><p>
-                Success!You may now {' '}
-                <Link to="/gardens">access Garden Buddy.</Link>
-              </p>
+              <>
+                <p>
+                  Success!You may now {' '}
+                  <Link to="/gardens">access Garden Buddy.</Link>
+                </p>
               </>
             ) : (
-              <form onSubmit={handleFormSubmit}>
+              <form onSubmit={handleFormSubmit} className='form-wrap'>
+                <label>Login</label>
                 <input
-                  className="form-input"
+                  className="login-input"
                   placeholder="Your email"
                   name="email"
                   type="email"
                   value={formState.email}
                   onChange={handleChange}
                 />
+                <label>Password</label>
                 <input
-                  className="form-input"
+                  className="login-input"
                   placeholder="******"
                   name="password"
                   type="password"
@@ -71,12 +74,12 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-info"
+                  className="login-btn"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                   to="/gardens"
                 >
-                  Submit
+                  Login
                 </button>
               </form>
             )}
@@ -88,7 +91,6 @@ const Login = (props) => {
             )}
           </div>
         </div>
-      </div>
     </main>
   );
 };
